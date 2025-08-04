99dfe976c2c8c69c8144881a46c54b4fd22c3954
Murilo Marinho <murilomarinho@manchester.ac.uk>
2025-08-04 16:24:41 +0100

    [install_gazebo.sh] Added missing -y trigger for apt.

--- install_gazebo.sh
+++ install_gazebo.sh
@@ -3,7 +3,7 @@ set -e
 
 # https://gazebosim.org/docs/harmonic/install_ubuntu/
 sudo apt-get update
-sudo apt-get install curl lsb-release gnupg
+sudo apt-get install -y curl lsb-release gnupg
 
 sudo curl https://packages.osrfoundation.org/gazebo.gpg --output /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
