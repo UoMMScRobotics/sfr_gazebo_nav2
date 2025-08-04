f4592dd9342c4b7fdee577a33a0ab84382e24e02
Murilo Marinho <murilomarinho@manchester.ac.uk>
2025-08-04 16:19:01 +0100

    Adding Dockerfile and install script.

---
+++ install_gazebo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# https://gazebosim.org/docs/harmonic/install_ubuntu/
+sudo apt-get update
+sudo apt-get install curl lsb-release gnupg
+
+sudo curl https://packages.osrfoundation.org/gazebo.gpg --output /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
+sudo apt-get update
+sudo apt-get install gz-harmonic
+
+# Remove APT files
+rm -rf /var/cache/apt/archives /var/lib/apt/lists/*
