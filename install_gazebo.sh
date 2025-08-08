43596132f82527b68055e3553215155bc48cf3a3
Murilo M Marinho <46012516+mmmarinho@users.noreply.github.com>
2025-08-08 13:35:00 +0100

    Update install_gazebo.sh

--- install_gazebo.sh
+++ install_gazebo.sh
@@ -14,21 +14,19 @@ sudo apt-get install -y gz-harmonic
 ## Install pairing ROS pairing libraries
 sudo apt-get install -y ros-jazzy-ros-gz
 
-## Create ros2_tutorial_workspace external if needed
+## Create ros2_tutorial_workspace external
 mkdir -p "$HOME"/ros2_tutorial_workspace/src/external
 
 ## Add ros_gz_project_template
-
-### Add env
-echo "# Add GZ_VERSION for ros_gz_project_template" > "$HOME"/.bashrc
-echo "export GZ_VERSION=harmonic" > "$HOME"/.bashrc
-
-### Clone and build
-export GZ_VERSION=harmonic
+#### Clone repository
 cd "$HOME"/ros2_tutorial_workspace/src/external
 git clone --depth 1 https://github.com/gazebosim/ros_gz_project_template.git
+#### Build 
 source /opt/ros/jazzy/setup.bash
+cd "$HOME"/ros2_tutorial_workspace
+export GZ_VERSION=harmonic
 colcon build
+rm -rf build log
 
 # Install nav2 (https://docs.nav2.org/getting_started/index.html)
 sudo apt-get install -y ros-jazzy-navigation2
@@ -37,3 +35,7 @@ sudo apt-get install -y ros-jazzy-nav2-minimal-tb*
 
 # Remove APT files
 rm -rf /var/cache/apt/archives /var/lib/apt/lists/*
+
+# Add env to bash_rc
+echo "# Add GZ_VERSION for ros_gz_project_template" > "$HOME"/.bashrc
+echo "export GZ_VERSION=harmonic" > "$HOME"/.bashrc
