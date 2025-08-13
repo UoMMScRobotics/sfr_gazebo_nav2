99b6215e0d49744025fb05b2a73a934c628ebf1d
Murilo M Marinho <46012516+mmmarinho@users.noreply.github.com>
2025-08-13 10:30:06 +0100

    Update install_gazebo.sh

--- install_gazebo.sh
+++ install_gazebo.sh
@@ -21,12 +21,6 @@ mkdir -p "$HOME"/ros2_tutorial_workspace/src/external
 #### Clone repository
 cd "$HOME"/ros2_tutorial_workspace/src/external
 git clone --depth 1 https://github.com/gazebosim/ros_gz_project_template.git
-#### Build 
-source /opt/ros/jazzy/setup.bash
-cd "$HOME"/ros2_tutorial_workspace
-export GZ_VERSION=harmonic
-colcon build
-rm -rf build log
 
 # Install nav2 (https://docs.nav2.org/getting_started/index.html)
 sudo apt-get install -y ros-jazzy-navigation2
@@ -36,9 +30,20 @@ sudo apt-get install -y ros-jazzy-nav2-minimal-tb*
 # Install MoveIt 2 (https://moveit.ai/install-moveit2/binary/)
 sudo apt-get install -y ros-jazzy-moveit
 
-# Remove APT files
-rm -rf /var/cache/apt/archives /var/lib/apt/lists/*
+## Clone moveit tutorials. There's currently no "jazzy" branch (https://github.com/moveit/moveit2_tutorials/issues/1033)
+cd "$HOME"/ros2_tutorial_workspace/src/external
+git clone -b main https://github.com/moveit/moveit2_tutorials
+
+# Build external examples
+source /opt/ros/jazzy/setup.bash
+cd "$HOME"/ros2_tutorial_workspace
+export GZ_VERSION=harmonic
+colcon build
+rm -rf build log
 
 # Add env to bash_rc
 echo "# Add GZ_VERSION for ros_gz_project_template" > "$HOME"/.bashrc
 echo "export GZ_VERSION=harmonic" > "$HOME"/.bashrc
+
+# Remove APT files
+rm -rf /var/cache/apt/archives /var/lib/apt/lists/*
