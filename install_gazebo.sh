332f457119dbc423cf7112de4ca1bd73f4f2cc5b
Murilo M Marinho <46012516+mmmarinho@users.noreply.github.com>
2025-11-10 09:30:14 +0000

    Make install_gazebo.sh install only Gazebo.
    Removed steps for creating workspace and installing nav2.

--- install_gazebo.sh
+++ install_gazebo.sh
@@ -14,29 +14,5 @@ sudo apt-get install -y gz-harmonic
 ## Install pairing ROS pairing libraries
 sudo apt-get install -y ros-jazzy-ros-gz
 
-## Create ros2_tutorial_workspace external
-mkdir -p "$HOME"/ros2_tutorial_workspace/src/external
-
-## Add ros_gz_project_template
-#### Clone repository
-cd "$HOME"/ros2_tutorial_workspace/src/external
-git clone --depth 1 https://github.com/gazebosim/ros_gz_project_template.git
-
-# Install nav2 (https://docs.nav2.org/getting_started/index.html)
-sudo apt-get install -y ros-jazzy-navigation2
-sudo apt-get install -y ros-jazzy-nav2-bringup
-sudo apt-get install -y ros-jazzy-nav2-minimal-tb*
-
-# Build external examples
-source /opt/ros/jazzy/setup.bash
-cd "$HOME"/ros2_tutorial_workspace
-export GZ_VERSION=harmonic
-colcon build
-rm -rf build log
-
-# Add env to bash_rc
-echo "# Add GZ_VERSION for ros_gz_project_template" > "$HOME"/.bashrc
-echo "export GZ_VERSION=harmonic" > "$HOME"/.bashrc
-
 # Remove APT files
 rm -rf /var/cache/apt/archives /var/lib/apt/lists/*
