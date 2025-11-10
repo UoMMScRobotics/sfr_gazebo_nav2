d77952df022e8410911ddc84189fef59de44ec7f
Murilo M Marinho <46012516+mmmarinho@users.noreply.github.com>
2025-11-10 09:28:09 +0000

    Add installation script for nav2
    This script installs the necessary packages for nav2 and cleans up APT files.

---
+++ install_nav2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+# Install nav2 (https://docs.nav2.org/getting_started/index.html)
+sudo apt-get install -y ros-jazzy-navigation2
+sudo apt-get install -y ros-jazzy-nav2-bringup
+sudo apt-get install -y ros-jazzy-nav2-minimal-tb*
+
+# Remove APT files
+rm -rf /var/cache/apt/archives /var/lib/apt/lists/*
