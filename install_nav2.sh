6f26b65a8cf3c1fdf33543090f60e5d252cccd62
Murilo M Marinho <46012516+mmmarinho@users.noreply.github.com>
2025-11-16 13:31:52 +0000

    Add apt-get update and upgrade commands
    Added commands to update and upgrade apt-get before installing nav2.

--- install_nav2.sh
+++ install_nav2.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+# Update and upgrade apt-get
+sudo apt-get update
+sudo apt-get upgrade -y
+
 # Install nav2 (https://docs.nav2.org/getting_started/index.html)
 sudo apt-get install -y ros-jazzy-navigation2
 sudo apt-get install -y ros-jazzy-nav2-bringup
