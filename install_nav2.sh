ce07dd13547c3967c107ecb9809a995677c95d62
Murilo M Marinho <46012516+mmmarinho@users.noreply.github.com>
2025-11-22 13:33:10 +0000

    [install_nav2.sh] Add packages used in official tutorials.
    Added installation commands for joint state publisher GUI, xacro, and slam toolbox, and reordered sdf-urdf converter installation.

--- install_nav2.sh
+++ install_nav2.sh
@@ -5,13 +5,20 @@ set -e
 sudo apt-get update
 sudo apt-get upgrade -y
 
-# Install the sdf-urdf converter (https://docs.nav2.org/setup_guides/sdf/setup_sdf.html)
-sudo apt-get install -y ros-jazzy-sdformat-urdf
-
 # Install nav2 (https://docs.nav2.org/getting_started/index.html)
 sudo apt-get install -y ros-jazzy-navigation2
 sudo apt-get install -y ros-jazzy-nav2-bringup
 sudo apt-get install -y ros-jazzy-nav2-minimal-tb*
+
+# (https://docs.nav2.org/setup_guides/urdf/setup_urdf.html)
+sudo apt-get install -y ros-jazzy-joint-state-publisher-gui
+sudo apt-get install -y ros-jazzy-xacro
+
+# (https://docs.nav2.org/setup_guides/sensors/mapping_localization.html)
+sudo apt-get install -y ros-jazzy-slam-toolbox
+
+# Install the sdf-urdf converter (https://docs.nav2.org/setup_guides/sdf/setup_sdf.html)
+sudo apt-get install -y ros-jazzy-sdformat-urdf
 
 # Remove APT files
 rm -rf /var/cache/apt/archives /var/lib/apt/lists/*
