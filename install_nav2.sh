51a4635fa0c881b6fe66efe2ccce2bddec866ece
Murilo M Marinho <46012516+mmmarinho@users.noreply.github.com>
2025-11-22 22:42:50 +0000

    Clean up install_nav2.sh by removing comments
    Removed commented out installation commands for URDF and SDF.

--- install_nav2.sh
+++ install_nav2.sh
@@ -10,15 +10,8 @@ sudo apt-get install -y ros-jazzy-navigation2
 sudo apt-get install -y ros-jazzy-nav2-bringup
 sudo apt-get install -y ros-jazzy-nav2-minimal-tb*
 
-# (https://docs.nav2.org/setup_guides/urdf/setup_urdf.html)
-# sudo apt-get install -y ros-jazzy-joint-state-publisher-gui
-# sudo apt-get install -y ros-jazzy-xacro
-
 # (https://docs.nav2.org/setup_guides/sensors/mapping_localization.html)
 sudo apt-get install -y ros-jazzy-slam-toolbox
-
-# Install the sdf-urdf converter (https://docs.nav2.org/setup_guides/sdf/setup_sdf.html)
-# sudo apt-get install -y ros-jazzy-sdformat-urdf
 
 # Remove APT files
 rm -rf /var/cache/apt/archives /var/lib/apt/lists/*
